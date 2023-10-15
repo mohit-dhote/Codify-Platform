6412af5bc76d7632f13c3c5bc6b383e791d397e5
Vishal Mishra <128444479+vishalm029@users.noreply.github.com>
2023-10-15 17:24:36 +0530

    Update add_two_numbers_represented_by_linked_lists.cpp
    Beats 95.71%of users with C++

--- Add Code Here/CPP/add_two_numbers_represented_by_linked_lists.cpp
+++ Add Code Here/CPP/add_two_numbers_represented_by_linked_lists.cpp
@@ -1,105 +1,52 @@
-#include<bits/stdc++.h>
-using namespace std;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode*dummy = new ListNode();
+        //a node pointing to dummy 
+        ListNode*temp=dummy;
+        int carry=0;
 
-struct Node{
+//at last when l1== NULL and l2==NULL and carry!=0 do one more iteration and add carry=1 as a node 
+        while(l1 != NULL || l2!= NULL || carry==1){
+            int sum=0;
+           
+           //sum from l1
+           if(l1!=NULL){
+               sum+=l1->val;
+               //move l1 to next node
+               l1=l1->next;
+           }
 
-    int data;
-    Node *next;
-    Node(int data){
-        this->data=data;
-        next=NULL;
+           //sum from l2
+           if(l2!=NULL){
+               sum+=l2->val;
+               //move l2 to next node
+               l2=l2->next;
+           }
+
+          //add sum  alongwith carry 
+          sum+=carry;
+          carry=sum/10;
+
+          //add sum%10 as a new node 
+          ListNode* node= new ListNode(sum%10);
+          //make temp point to new node created
+          temp->next = node ; 
+          //move temp to next position
+          temp=temp->next;
+        }
+
+        //now our answer is node from duumy->next to last 
+        return dummy->next;
     }
 };
-
-void printList(struct Node *head){
-    struct Node *p = head;
-
-    while(p!=NULL){
-        cout<<p->data<<" ";
-        p=p->next;
-    }
-}
-
-Node *reverse(struct Node *head){
-
-    struct Node *cur = head;
-    Node *nex;
-    Node *prev=NULL;
-
-    while(cur!=NULL){
-        nex=cur->next;
-        cur->next=prev;
-        prev=cur;
-        cur=nex;
-    }
-    head = prev;
-
-    return head;
-}
-
-Node *addTwoList(struct Node *first ,struct Node *second){
-
-    first = reverse(first);
-    second = reverse(second);
-
-    int c=0;
-    int s=0;
-    struct Node *temp;
-    struct Node *cur=NULL;
-    struct Node *res=NULL;
-
-    while(first!=NULL || second!=NULL){
-
-        s = c + (first?first->data:0) + (second?second->data:0);
-        c = s>=10?1:0;
-        s = s%10;
-
-        Node *temp = new Node(s);
-        if(res==NULL) res=temp;
-        else cur->next=temp;
-        cur=temp;
-
-        if(first) first=first->next;
-        if(second) second=second->next;
-
-    }
-
-    if(c>0){
-        Node *temp = new Node(c);
-        cur->next=temp;
-        cur=temp;
-    }
-
-
-    res = reverse(res);
-
-    return res;
-
-}
-
-int main(){
-    
-    
-    Node* first = new Node(2);
-    first->next = new Node(3);
-    first->next->next = new Node(4);
-    
-    Node* second = new Node(3);
-    second->next = new Node(4);
-    second->next->next = new Node(5);
-
-    cout<<"First list = ";
-    printList(first);
-
-    cout<<"\nSecond list = ";
-    printList(second);
-
-    Node *ans = addTwoList(first ,second);
-
-    cout<<"\nAdd Two list = ";
-
-    printList(ans);
-    
-
-    return 0;
-}
