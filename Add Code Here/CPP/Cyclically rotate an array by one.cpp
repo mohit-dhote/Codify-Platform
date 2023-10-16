a7ee93ee272400f68b64d8009c9639614f28a0b7
Vishal Mishra <128444479+vishalm029@users.noreply.github.com>
2023-10-16 20:12:47 +0530

    Update Cyclically rotate an array by one.cpp
    Beats 83.33%of users with C++

--- Add Code Here/CPP/Cyclically rotate an array by one.cpp
+++ Add Code Here/CPP/Cyclically rotate an array by one.cpp
@@ -1,35 +1,27 @@
- // Q. Given an array, rotate the array by one position in clock-wise direction.
- 
-
-// Example 1:
-
-// Input:
-// N = 5
-// A[] = {1, 2, 3, 4, 5}
-// Output:
-// 5 1 2 3 4
-
-
-
-
-#include <bits/stdc++. h>
-using namespace std;
-
-int main() {
-    int n;
-    cin>>n;
-    for(int i=0;i<n;i++)
-    {
-        int s;
-        cin>>s;
-        int a[s];
-        for(int j=0;j<s;j++)
-        cin>>a[j];
-        cout<<a[s-1]<<" ";
-        for(int j=0;j<s-1;j++)
-        cout<<a[j]<<" ";
-        cout<<endl;
+class Solution {
+public:
+  vector<vector<int>> rotateGrid(vector<vector<int>>& grid, int k) {
+    int y = grid.size(), x = grid[0].size();
+    int yend = y - 1, xend = x - 1;
+    int limit = min(y>>1, x>>1), n = (yend<<1) + (xend<<1);
+    vector<int> rot(n+n);
+       
+    for(int i = 0; i != limit; i++, n -= 8, xend--, yend--){
+      int tk = k % n;
+      if(tk){
+        int k1 = 0, k2 = n;
+        for(int r =    i; r < yend;  r++) rot[k1++] = rot[k2++] = grid[r][i];
+        for(int c =    i; c < xend;  c++) rot[k1++] = rot[k2++] = grid[yend][c];
+        for(int r = yend; r >    i;  r--) rot[k1++] = rot[k2++] = grid[r][xend];
+        for(int c = xend; c >    i;  c--) rot[k1++] = rot[k2++] = grid[i][c];
+        tk = n - tk;
+        for(int r =    i; r < yend;  r++) grid[r][i]    = rot[tk++];
+        for(int c =    i; c < xend;  c++) grid[yend][c] = rot[tk++];
+        for(int r = yend; r >    i;  r--) grid[r][xend] = rot[tk++];
+        for(int c = xend; c >    i;  c--) grid[i][c]    = rot[tk++];
+      }
     }
-	//code
-	return 0;
-}
+    
+    return grid;
+  }
+};
